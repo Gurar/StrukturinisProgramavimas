8e6b9ed884ccb9b566ffea11d46932381d466b95
Gurar <jezy.kozlovski@gmail.com>
2024-09-13 09:04:51 +0300

    antra uzzduotis

--- PracticalTask/PracticalTask.cpp
+++ PracticalTask/PracticalTask.cpp
@@ -26,6 +26,22 @@ int main()
         "Grupie: " << grupie << "\n"
         "Kursas: " << kursas << "\n"
         "Studiu programos pavadinimas: " << dalykoPavadinimas << "\n" << endl;
+   
+    // Antras uzduotis
 
+    string
+        kluboPavadinimas = "Vilniaus Rytas",
+        kluboSavininkas = "Jonas Jonaitis",
+        stadionas = "Vilniaus stadionas";
+    int
+        ikurimoMetai = 1990,
+        vietuSkaicius = 400;
+
+    cout
+        << "Klubo Pavadinimas: " << kluboPavadinimas << "\n"
+        << "kluboSavininkas: " << kluboSavininkas << "\n"
+        << "Stadionas: " << stadionas << "\n"
+        << "Ikurimo metai: " << ikurimoMetai << "\n"
+        << "Vietu skaicius: " << vietuSkaicius << endl;
 }
 
