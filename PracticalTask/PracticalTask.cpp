7ec2e94076c4cbd371cf40409b9c227a64db541e
Gurar <jezy.kozlovski@gmail.com>
2024-09-13 09:03:04 +0300

    praktine uzduotis 1

---
+++ PracticalTask/PracticalTask.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    // Pirma uzduotis 
+    string
+        vardas = "Jezy",
+        pavarde = "Kozlovski",
+        grupie = "Pirma",
+        kursas = "Pirmas kursas",
+        dalykoPavadinimas = "strukturins programavimas";
+
+    int amzius = 34;
+
+    cout <<
+        "Vardas: " << vardas << "\n"
+        "Pavarde: " << pavarde << "\n"
+        "Amzius: " << amzius << "\n"
+        "Grupie: " << grupie << "\n"
+        "Kursas: " << kursas << "\n"
+        "Studiu programos pavadinimas: " << dalykoPavadinimas << "\n" << endl;
+
+}
+
