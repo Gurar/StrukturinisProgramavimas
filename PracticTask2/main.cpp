093190ce0722d88ec6a5d9106ee38c12ae4ec43e
Gurar <jezy.kozlovski@gmail.com>
2024-09-19 11:39:16 +0300

    praktine uzduotis 3

--- PracticTask2/main.cpp
+++ PracticTask2/main.cpp
@@ -7,15 +7,33 @@ void main() {
 
 	// Pirma ir Antra praktine uzduotis
 	int
-		pz1 = 3,
+		pz1 = 10,
 		pz2 = 10,
-		pz3 = 7,
-		pz4 = 2,
-		pz5 = 2;
+		pz3 = 10,
+		pz4 = 10,
+		pz5 = 10;
 
 	float vidurkis = static_cast<float>(pz1 + pz2 + pz3 + pz4 + pz5) / 5;
 
 	cout << "vidurkis: " << vidurkis << " " << (vidurkis >= 5 ? "Teigiams vidurkis" : " Neigiamas vidurkis") << endl;
 	
 	cout << "====================" << endl;
+
+	// Trecia uzdiotis
+
+	if (vidurkis == 10) {
+		cout << "Puiku" << endl;
+	}
+	else if (vidurkis >= 9) {
+		cout << "Labai Gerai" << endl;
+	}
+	else if (vidurkis >= 7) {
+		cout << "Gerai" << endl;
+	}
+	else if (vidurkis >= 5) {
+		cout << "Patenkinamai" << endl;
+	}
+	else {
+		cout << "Egzamina ne islaikitas" << endl;
+	}
 }
