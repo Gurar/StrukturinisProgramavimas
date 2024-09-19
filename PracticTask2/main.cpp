569e34060f66df33cd7b0a214e5f368e23856476
Gurar <jezy.kozlovski@gmail.com>
2024-09-19 11:03:20 +0300

    Praktine uzduotis 1 ir 2

---
+++ PracticTask2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+void main() {
+
+	int
+		pz1 = 3,
+		pz2 = 10,
+		pz3 = 7,
+		pz4 = 2,
+		pz5 = 2;
+
+	float vidurkis = static_cast<float>(pz1 + pz2 + pz3 + pz4 + pz5) / 5;
+
+	cout << "vidurkis: " << vidurkis << " " << (vidurkis >= 5 ? "Teigiams vidurkis" : " Neigiamas vidurkis");
+	
+}
