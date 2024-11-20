a0bc17e5af7e34ecc59a6b13ba0050094fdf6b73
Gurar <Gurar@DESKTOP-UTSQC1P>
2024-11-20 17:32:38 +0200

    Sukurta sifravimuo meniu ir ivedimas zodi ir raktini zodi

---
+++ treciaPraktineUzduotis/treciaPraktineUzduotis.cpp
@@ -0,0 +1,44 @@
+﻿// treciaPraktineUzduotis.cpp : Defines the entry point for the application.
+//
+
+#include "treciaPraktineUzduotis.h"
+
+using namespace std;
+
+string input(string message) {
+	string input;
+	cout << message << endl;
+	cin >> input;
+	return input;
+}
+
+int main()
+{
+	int command;
+	string str, strKey;
+	while (true) {
+		str = input("Iveskite zodi");
+		strKey = input("Iveskite raktini zuodi");
+		cout
+			<< "Pasirinkite operacija \n"
+			<< "1 - Sifravimas nauduojant masivo abecelie \n"
+			<< "2 - Sifravimas nauduojant ASCII lentele \n"
+			<< "0 - Baigti darba" << endl;
+		cin >> command;
+
+		if (command == 0)
+			break;
+
+		switch (command) {
+			case 1:
+				cout << "1" << endl;
+				break;
+			case 2:
+				cout << "3" << endl;
+				break;
+		}
+	}
+
+
+	return 0;
+}
---
+++ treciaPraktineUzduotis/treciaPraktineUzduotis.h
@@ -0,0 +1,10 @@
+﻿// treciaPraktineUzduotis.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <clocale>
+
+// TODO: Reference additional headers your program requires here.
