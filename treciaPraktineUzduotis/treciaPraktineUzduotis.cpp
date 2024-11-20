08d1b6ba31279c4e55958007812a0cd577a938f9
Gurar <Gurar@DESKTOP-UTSQC1P>
2024-11-20 19:04:36 +0200

    Sukurtas sifravimos ir desifravimas nauduojant masivo abecele

--- treciaPraktineUzduotis/treciaPraktineUzduotis.cpp
+++ treciaPraktineUzduotis/treciaPraktineUzduotis.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+char ABECELE[52]{};
+
 string input(string message) {
 	string input;
 	cout << message << endl;
@@ -12,13 +14,69 @@ string input(string message) {
 	return input;
 }
 
+void abecele() {
+	for (int i = 0; i < 26; i++) {
+		ABECELE[i] = 'A' + i;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		ABECELE[26 + i] = 'a' + i;
+	}
+}
+
+void sifravimas(string str, string strKey) {
+	int m, k;
+
+	for (int i = 0; i < str.size(); i++) {
+		for (int j = 0; j < 52; j++) {
+			if (str[i] == ABECELE[j]) {
+				m = j;
+				break;
+			}
+		}
+
+		for (int j = 0; j < 52; j++) {
+			if (strKey[i % strKey.size()] == ABECELE[j]) {
+				k = j;
+				break;
+			}
+		}
+
+		str[i] = ABECELE[(m + k) % 52];
+	}
+
+	cout << "Uzsifruotas tekstas: " << str << endl;
+
+	for (int i = 0; i < str.size(); i++) {
+		for (int j = 0; j < 52; j++) {
+			if (str[i] == ABECELE[j]) {
+				m = j;
+				break;
+			}
+		}
+
+		for (int j = 0; j < 52; j++) {
+			if (strKey[i % strKey.size()] == ABECELE[j]) {
+				k = j;
+				break;
+			}
+		}
+
+		str[i] = ABECELE[(m - k + 52) % 52];
+	}
+
+	cout << "Desifruotas tekstas: " << str << endl;
+
+}
+
 int main()
 {
+	abecele();
 	int command;
 	string str, strKey;
 	while (true) {
 		str = input("Iveskite zodi");
-		strKey = input("Iveskite raktini zuodi");
+		strKey = input("Iveskite raktini zodi");
 		cout
 			<< "Pasirinkite operacija \n"
 			<< "1 - Sifravimas nauduojant masivo abecelie \n"
@@ -31,10 +89,10 @@ int main()
 
 		switch (command) {
 			case 1:
-				cout << "1" << endl;
+				cout << "Ivestas tekstas: " << str << endl;
+				sifravimas(str, strKey);
 				break;
 			case 2:
-				cout << "3" << endl;
 				break;
 		}
 	}
